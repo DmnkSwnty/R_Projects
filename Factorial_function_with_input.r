c71cd75b535557e6eff667c618d4ee2e3074a48b
Dominik Swienty <dominik.swienty@outlook.de>
2022-04-10 16:54:31 +0200

    Changes

--- Factorial_function_with_input.r
+++ Factorial_function_with_input.r
@@ -1,4 +1,5 @@
-input = readline(prompt="Please input your number: ")
 
-fact = function(input){if (input == 0){return(1)} else return (fact(input - 1) * input)}
+
+fact = function(input){input = readline(prompt="Please input your number") if (input == 0){return(1)} else return (fact(input - 1) * input)}
+
 print(fact(input))
