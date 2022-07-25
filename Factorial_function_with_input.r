b90ff96c4a38ba6f060d7c0d79df73c2c652a010
Dominik Swienty <dominik.swienty@outlook.de>
2022-07-25 14:35:02 +0200

    worked on factorial function

--- Factorial_function_with_input.r
+++ Factorial_function_with_input.r
@@ -1,6 +1,5 @@
 
 
-<<<<<<< HEAD
 fact <- function(n) {
     if (n == 0) {
         return(1)
@@ -8,8 +7,3 @@ fact <- function(n) {
         return(fact(n - 1) * n)
     }
 }
-=======
-fact = function(x){if (x == 0){return (1)} else return(fact(x-1) * x)}
-
-fact(5)
->>>>>>> cef39c2368ecbbbb4452b31fd591afbf72d20e00
